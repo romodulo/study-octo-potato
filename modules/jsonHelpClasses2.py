57a3dac44a5b84d3ebb02a3773704700de62d60f
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-01-05 16:48:24 +1000

    stable

---
+++ modules/jsonHelpClasses2.py
@@ -0,0 +1,13 @@
+import json
+
+
+
+
+if __name__ == '__main__':
+    print(dir(json))
+    variable = dir(json)
+    lenVariable = len(variable)
+    intVariable = lenVariable/10
+    if lenVariable % 10 > 0:
+        pass
+        # print("GOODGOODGOODGOODGOODGOODGOOD")
